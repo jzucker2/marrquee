9fff50fb0568afb5cc0e793b8a1d193d6ef50c01
Jordan Zucker <jordan.zucker@gmail.com>
2025-05-22 13:04:21 -0700

    Another small refactor

--- app/plex.py
+++ app/plex.py
@@ -11,16 +11,24 @@ BASE_URL = os.environ.get("PLEX_BASE_URL")
 TOKEN = os.environ.get("PLEX_TOKEN")
 
 
-log.debug(f'Plex BASE_URL: {BASE_URL}')
-
-
-plex = PlexServer(BASE_URL, TOKEN)
-
-
 class PlexClient:
+    _plex = None
+
+    @classmethod
+    def _get_plex(cls):
+        if cls._plex is None:
+            try:
+                log.debug(f'Plex BASE_URL: {BASE_URL}')
+                cls._plex = PlexServer(BASE_URL, TOKEN)
+                log.debug("Initialized PlexServer client.")
+            except Exception as e:
+                log.error(f"Failed to initialize PlexServer: {e}")
+                raise
+        return cls._plex
+
     @classmethod
     def get_random_movie_poster(cls):
-        movies = plex.library.section('Movies').all()
+        movies = cls._get_plex().library.section('Movies').all()
         random_movie = random.choice(movies)
         log.debug(f'Plex random_movie: {random_movie}')
         poster_url = f"{BASE_URL}{random_movie.thumb}?X-Plex-Token={TOKEN}"
@@ -34,7 +42,7 @@ class PlexClient:
     @classmethod
     def get_manual_movie_poster(cls, movie_title):
         try:
-            movie = plex.library.section('Movies').get(movie_title)
+            movie = cls._get_plex().library.section('Movies').get(movie_title)
             log.debug(f'Plex movie: {movie}')
             poster_url = f"{BASE_URL}{movie.thumb}?X-Plex-Token={TOKEN}"
             log.debug(f'Plex movie: {movie} => poster_url: {poster_url}')
